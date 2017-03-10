977a37f8131f35befea940c91ed0b2aff739a0cc
Wioleta P <przybyszwioleta@gmail.com>
2017-03-10 17:08:07 +0100

    javascript

---
+++ 1/app.js
@@ -0,0 +1,8 @@
+var addBtn = document.querySelector(".add-employee-btn");
+addBtn.addEventListener("click", function(){
+    var li = document.createElement("li");
+    var list = document.querySelector(".employee-list");
+    var input = document.querySelector(".employee-name-input");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+}), (false);
