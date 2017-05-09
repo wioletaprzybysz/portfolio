949abb86534c275724a66f151330b3bd35033c70
Wioleta <przybyszwioleta@gmail.com>
2017-05-09 21:10:39 +0200

    Add files via upload

---
+++ portfolio_W_Przybysz/js/script.js
@@ -0,0 +1,72 @@
+ $(document).ready(function(){
+   $('li').click(function(){
+    $('li').removeClass('active');
+    $(this).addClass('active');
+});
+     
+     // menu mini
+     
+ $(document).ready(function() {
+     $("#navigation-mobile").hide();
+     $("#navigation-mobile").fadeOut(300);
+        $(".open").click(function(){
+        $("#navigation-mobile").show(100);
+    });
+    $(".close").click(function(){
+        $("#navigation-mobile").hide(100);
+    });
+});
+     
+     
+ 
+     
+     // Cache selectors
+var lastId,
+    topMenu = $(".desktop-menu"),
+    topMenuHeight = topMenu.outerHeight()+-15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+});
+ 
+	
+	
+ 
